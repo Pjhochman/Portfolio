25de3e1b213c04e2cf62473aa814254e39e999b4
Peter Hochman <peter.hochman@hyperisland.se>
2021-04-12 09:01:20 +0200

    removed unnecessary lines of code

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,21 +15,7 @@ const Footer = () => {
   return (
     <>
       <div style={width} className={hover}></div>
-      <footer>
-        {/* <div id="designer" onMouseEnter={onHover} onMouseLeave={onLeave}>
-          <h2 className="title">designer</h2>
-          <p className="description">
-            Passion for designing beautiful and functional experiences.
-          </p>
-        </div>
-        <div id="coder">
-          <h2 className="title">&#60;coder&#62;</h2>
-          <p className="description">
-            Front End Developer who focuses on writing clean, elegant and
-            efficient code.
-          </p>
-        </div> */}
-      </footer>
+      <footer></footer>
     </>
   );
 };
