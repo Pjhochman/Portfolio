d43d38932e1e7f225cf9616600556c43886ab4ae
Peter Hochman <peter.hochman@hyperisland.se>
2021-04-12 09:07:53 +0200

    removed unnecessary lines of code

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Footer = () => {
-  const [hover, setHover] = useState("colorPalette");
-  const [width, setWidth] = useState({ width: "0vw" });
+  //   const [hover, setHover] = useState("colorPalette");
+  //   const [width, setWidth] = useState({ width: "0vw" });
 
-  const onHover = () => {
-    setHover("colorPalette active");
-    setWidth({ width: "50vw" });
-  };
-  const onLeave = () => {
-    setHover("colorPalette active");
-    setWidth({ width: "0vw" });
-  };
+  //   const onHover = () => {
+  //     setHover("colorPalette active");
+  //     setWidth({ width: "50vw" });
+  //   };
+  //   const onLeave = () => {
+  //     setHover("colorPalette active");
+  //     setWidth({ width: "0vw" });
+  //   };
   return (
     <>
-      <div style={width} className={hover}></div>
-      <footer></footer>
+      {/* <div style={width} className={hover}></div> */}
+      <footer>
+        {/* <div id="designer" onMouseEnter={onHover} onMouseLeave={onLeave}>
+          <h2 className="title">designer</h2>
+          <p className="description">
+            Passion for designing beautiful and functional experiences.
+          </p>
+        </div>
+        <div id="coder">
+          <h2 className="title">&#60;coder&#62;</h2>
+          <p className="description">
+            Front End Developer who focuses on writing clean, elegant and
+            efficient code.
+          </p>
+        </div> */}
+      </footer>
     </>
   );
 };
