2635fa75d12928e47c582e1844c3e5bbba34c826
Peter Hochman <peter.hochman@hyperisland.se>
2021-04-16 13:12:35 +0200

    fix/removed warning

--- src/components/Cards/CardList/Card4.js
+++ src/components/Cards/CardList/Card4.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
 import { fadeInRight } from "react-animations";
 import { fadeInLeft } from "react-animations";
