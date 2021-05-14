29ef4f75dc902e7908276ba7d43c88d9250bbcdb
Peter Hochman <peter.hochman@hyperisland.se>
2021-05-14 19:55:20 +0200

    feature/added sitemap.xml

---
+++ routes.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Route } from "react-router";
+
+export default (
+  <Route>
+    <Route path="/my-app/src/components/Aside/Aside.js" />
+    <Route path="/about/:id" />
+  </Route>
+);
