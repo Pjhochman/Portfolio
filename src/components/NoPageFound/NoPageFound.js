c75b04fa9c0a16d9a2562815ad30c2d0833ad10b
Peter Hochman <peter.hochman@hyperisland.se>
2021-05-18 15:41:40 +0200

    fix/customized 404 page

--- src/components/NoPageFound/NoPageFound.js
+++ src/components/NoPageFound/NoPageFound.js
@@ -4,6 +4,7 @@ import "./styles/nopagefound.css";
 import { textColor } from "../../styles/GlobalStyles";
 
 const StyledWrapper = styled.div`
+  user-select: none;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -24,16 +25,35 @@ const StyledTitle = styled.h1`
 `;
 
 const StyledDescription = styled.h2`
-  font-size: 37px;
+  font-size: ${({ fontSize }) => fontSize};
   font-weight: 300;
   color: ${textColor.secondaryBlack};
+`;
+
+const StyledLink = styled.a`
+  font-size: 12px;
+  font-weight: bold;
+  padding: 15px;
+  margin-top: 20px;
+  text-transform: uppercase;
+  color: ${textColor.secondaryBlack};
+  border-radius: 20px;
+  box-shadow: -5px -5px 20px #fff, 5px 5px 20px #babecc;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    box-shadow: -2px -2px 5px #fff, 2px 2px 5px #babecc;
+  }
 `;
 
 const NoPageFound = () => {
   return (
     <StyledWrapper>
       <StyledTitle className="text">404</StyledTitle>
-      <StyledDescription> Page not found</StyledDescription>
+      <StyledDescription fontSize="37px"> Page not found</StyledDescription>
+      <StyledDescription fontSize="20px">
+        Sorry, the page you're looking for doesn't exist.
+      </StyledDescription>
+      <StyledLink href="/home">return home</StyledLink>
     </StyledWrapper>
   );
 };
